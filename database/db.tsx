e67d38cd774d2b8b7f72b13ef18f75311da5422a
Kenny <22737987+KennyLMQ@users.noreply.github.com>
2022-11-22 11:03:18 +0800

    build: update configurations

--- database/db.tsx
+++ database/db.tsx
@@ -1,5 +1,8 @@
 import { Pool } from "pg";
 
-const connectionString = process.env.DATABASE_LOGIN_URL;
-
-export const pool = new Pool({ connectionString });
+export const pool = new Pool({
+  user: process.env.POSTGRES_USER,
+  host: process.env.POSTGRES_HOST,
+  database: process.env.POSTGRES_DB,
+  password: process.env.POSTGRES_PASSWORD,
+});
