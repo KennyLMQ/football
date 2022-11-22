cbff482b9c2a89a8fe76ae88d9e987febeb6dab4
Kenny <22737987+KennyLMQ@users.noreply.github.com>
2022-11-22 11:25:03 +0800

    feat: update Link component
    Following this: https://github.com/mui/material-ui/commit/429404b2fdca86b89011c64944c5f5916a300594

--- components/Link.tsx
+++ components/Link.tsx
@@ -22,8 +22,17 @@ export const NextLinkComposed = React.forwardRef<
   HTMLAnchorElement,
   NextLinkComposedProps
 >(function NextLinkComposed(props, ref) {
-  const { to, linkAs, replace, scroll, shallow, prefetch, locale, ...other } =
-    props;
+  const {
+    to,
+    linkAs,
+    replace,
+    scroll,
+    shallow,
+    prefetch,
+    legacyBehavior = true,
+    locale,
+    ...other
+  } = props;
 
   return (
     <NextLink
@@ -35,7 +44,7 @@ export const NextLinkComposed = React.forwardRef<
       shallow={shallow}
       passHref
       locale={locale}
-      legacyBehavior
+      legacyBehavior={legacyBehavior}
     >
       <Anchor ref={ref} {...other} />
     </NextLink>
@@ -62,6 +71,7 @@ const Link = React.forwardRef<HTMLAnchorElement, LinkProps>(function Link(
     as,
     className: classNameProps,
     href,
+    legacyBehavior,
     linkAs: linkAsProp,
     locale,
     noLinkStyle,
@@ -99,6 +109,7 @@ const Link = React.forwardRef<HTMLAnchorElement, LinkProps>(function Link(
     scroll,
     shallow,
     prefetch,
+    legacyBehavior,
     locale,
   };
 
